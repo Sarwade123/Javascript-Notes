06392dec0e5b7e68218685e9673e396479376a8c
Rohit <rohitsarwade195@gmail.com>
2024-10-23 09:53:45 +0530

    added conditional statements

---
+++ 23-10-24/script.js
@@ -0,0 +1,64 @@
+// let age = prompt("Enter Your Age...")
+// // console.log(age);
+// if(age>=18){
+//     console.log('You are eligible for the driving...');
+    
+// }else{
+//     console.log(`You can get driving license after ${18-age} year`);
+    
+// }
+
+// let isDeposit = confirm("Do you want to Deposit money...?");
+// console.log(isDeposit);
+// let savingBal = 758495;
+// let currentBal = 84893793;
+// let ccBal = 59737084;
+// if(isDeposit){
+//     console.log("go to bank and deposit money. Online deposit system is curently not working");
+    
+// }
+// if(!isDeposit){
+//     let isWithdrawal = confirm("Do you want to withdraw money...?");
+//     if(isWithdrawal){
+//         console.log("Welcome to Withdraw section");
+        
+//     }else{
+//         let isBalanceCheck = confirm("Do you want to check account balance...?");
+//         if(isBalanceCheck){
+//             let optionOfCheckBalance = parseInt(
+//                 prompt(`Select your account type...!
+//                     1. Saving
+//                     2.Current
+//                     3.CC`)
+//             );
+//             console.log(optionOfCheckBalance);
+//             switch(optionOfCheckBalance){
+//                 case 1:
+//                     console.log(`Balance in your Saving account is ${savingBal}`);
+//                     break;
+
+//                 case 2:
+//                     console.log(`Balance in your current account is ${currentBal}`);
+//                     break;
+
+//                 case 3:
+//                     console.log(`Balance in your Saving account is ${ccBal}`);
+                    
+                    
+                    
+//             }
+            
+//         }
+//     }
+
+// }
+
+let arr = [10, 20, 30, 40];
+for(let i = 0; i < arr.length; i++){
+    console.log(arr[i]);
+    
+}
+
+let arr = [10, 20, 30, 40];
+let i = 0;
+while(i<arr.lenth)
